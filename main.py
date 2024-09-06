21204f1b915f71fe999571f00e71ff464ea4936c
maks5s <mskrypnyk0@gmail.com>
2024-09-06 11:49:49 +0300

    Created "Deck" class

--- main.py
+++ main.py
@@ -5,3 +5,27 @@ class Card:
 
     def __str__(self):
         return f"{self.rank} of {self.suit}"
+
+
+class Deck:
+    def __init__(self):
+        self.cards = []
+        suits = ["Hearts", "Diamonds", "Clubs", "Spades"]
+        ranks = [{"rank": "A", "value": 11},
+                 {"rank": "2", "value": 2},
+                 {"rank": "3", "value": 3},
+                 {"rank": "4", "value": 4},
+                 {"rank": "5", "value": 5},
+                 {"rank": "6", "value": 6},
+                 {"rank": "7", "value": 7},
+                 {"rank": "8", "value": 8},
+                 {"rank": "9", "value": 9},
+                 {"rank": "10", "value": 10},
+                 {"rank": "J", "value": 10},
+                 {"rank": "Q", "value": 10},
+                 {"rank": "K", "value": 10},]
+
+        for suit in suits:
+            for rank in ranks:
+                self.cards.append(Card(suit, rank))
+
