bd57aef6c3091d50c6381f5a2b6495faf1bb73e7
maks5s <mskrypnyk0@gmail.com>
2024-09-06 12:14:47 +0300

    Added "add_card" and "calculate_value" methods to the "Hand" class

--- main.py
+++ main.py
@@ -50,3 +50,19 @@ class Hand:
         self.value = 0
         self.dealer = dealer
 
+    def add_card(self, card_list):
+        self.cards.extend(card_list)
+
+    def calculate_value(self):
+        self.value = 0
+        has_ace = False
+
+        for card in self.cards:
+            card_value = int(card.rank["value"])
+            self.value += card_value
+
+            if card.rank["rank"] == "A":
+                has_ace = True
+
+        if has_ace and self.value > 21:
+            self.value -= 10
